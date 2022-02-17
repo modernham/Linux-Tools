733b71044976cf3d84d386ab6b98afeb1cc6aa48
modernham <billy.dean.penley@gmail.com>
2022-02-16 23:08:03 -0700

    Changes

--- LDAP/LDAP Client/AddDomainAndConfigure.sh
+++ LDAP/LDAP Client/AddDomainAndConfigure.sh
@@ -14,7 +14,10 @@ sudo wget -P /etc/sssd 192.168.1.2/sssd.conf
 sudo chmod 600 -R /etc/sssd
 sudo wget -P /usr/local/bin 192.168.1.2/fetchSSHKeysFromLDAP
 sudo chmod +x /usr/local/bin/fetchSSHKeysFromLDAP
+sudo sed -i 's/\r//' /usr/local/bin/fetchSSHKeysFromLDAP
 sudo echo -e "\nAuthorizedKeysCommand /usr/local/bin/fetchSSHKeysFromLDAP\nAuthorizedKeysCommandUser nobody\n" >> /etc/ssh/sshd_config
+sudo sed -i.bak '/mkhome/d' /etc/pam.d/common-session
+sudo echo -e "session required        pam_mkhomedir.so skel=/etc/skel/ umask=0022\n" >> /etc/pam.d/common-session
 sudo systemctl restart sssd
 sudo systemctl restart sshd
 echo Done!
